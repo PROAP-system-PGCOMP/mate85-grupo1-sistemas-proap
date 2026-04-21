6b02f9fc031abfb8f503d331b9e23b04e0134708
Igor Reis <igorreis377@gmail.com>
2026-04-21 15:05:34 -0300

    fixdouble/

--- proap-api/src/main/java/br/ufba/proap/configuration/SpaController.java
+++ proap-api/src/main/java/br/ufba/proap/configuration/SpaController.java
@@ -7,7 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 @Controller
 public class SpaController {
 
-    @RequestMapping(value = { "/{path:[^\\.]*}" })
+    @RequestMapping(value = { "/{path:[^\\.]*}", "/**/{path:[^\\.]*}"})
     public String redirect(HttpServletRequest request) {
         String path = request.getRequestURI();
         // Se a URL começar com /api, não faz forward, deixa dar 404 de verdade

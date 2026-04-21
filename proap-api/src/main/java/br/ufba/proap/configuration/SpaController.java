424fcb62890b7b510f305b8868b61d0b07f1a874
Igor Reis <igorreis377@gmail.com>
2026-04-21 16:29:56 -0300

    fixThymeleaf

--- proap-api/src/main/java/br/ufba/proap/configuration/SpaController.java
+++ proap-api/src/main/java/br/ufba/proap/configuration/SpaController.java
@@ -2,13 +2,19 @@ package br.ufba.proap.configuration;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class SpaController {
-    
-    @GetMapping(value = "/**/{path:[^\\.]*}")
+
+    // No Spring 3, para pegar tudo, usamos esta regex no path variable
+    @GetMapping(value = "/{path:[^\\.]*}")
     public String redirect() {
+        return "forward:/index.html";
+    }
+
+    // Para garantir rotas com "duas barras" como /usuario/perfil
+    @GetMapping(value = "/*/{path:[^\\.]*}")
+    public String redirectDeep() {
         return "forward:/index.html";
     }
 }

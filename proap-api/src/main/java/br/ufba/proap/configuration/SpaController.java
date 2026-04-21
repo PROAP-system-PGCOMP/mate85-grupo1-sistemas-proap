ec34612502dca882631907d26010d5a938c132f4
Igor Reis <igorreis377@gmail.com>
2026-04-21 15:17:06 -0300

    fix502

--- proap-api/src/main/java/br/ufba/proap/configuration/SpaController.java
+++ proap-api/src/main/java/br/ufba/proap/configuration/SpaController.java
@@ -4,16 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import jakarta.servlet.http.HttpServletRequest;
 
+
 @Controller
 public class SpaController {
 
-    @RequestMapping(value = { "/{path:[^\\.]*}", "/**/{path:[^\\.]*}"})
+    @RequestMapping(value = { "{path:[^\\.]*}", "/**/{path:[^\\.]*}" })
     public String redirect(HttpServletRequest request) {
         String path = request.getRequestURI();
-        // Se a URL começar com /api, não faz forward, deixa dar 404 de verdade
-        if (path.startsWith("/api")) {
+
+        if (path.startsWith("/api") || path.startsWith("/index.html") || path.startsWith("/assets")) {
             return "forward:/error";
         }
+
         return "forward:/index.html";
     }
-}
+}}

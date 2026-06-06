1e3c1f5d0631d92c5516aa8e476a04f97090d302
Nadson Santana <nadson.sousa@ufba.br>
2026-06-06 10:19:23 -0300

    feat: group request tables

--- proap-web/src/containers/home/solicitation-table/SolicitationTable.tsx
+++ proap-web/src/containers/home/solicitation-table/SolicitationTable.tsx
@@ -1,32 +1,10 @@
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
+import React from 'react';
 import SolicitationTableRequests from './SolicitationTableRequests';
-import SolicitationTableExtraRequests from './SolicitationTableExtraRequests';
-import { useState } from 'react';
-// Importação do tipo necessária para o TypeScript
-import { SolicitationDetailsDialogProps } from '../request-dialog/SolicitationDetailsDialog';
 
-interface SolicitationTableProps {
-  onShowDetails: (props: SolicitationDetailsDialogProps) => void;
-}
-
-export default function SolicitationTable({ onShowDetails }: SolicitationTableProps) {
-  const [currentTab, setCurrentTab] = useState(0);
-
+export default function SolicitationTable() {
   return (
     <>
-      <Tabs value={currentTab} onChange={(e, value) => setCurrentTab(value)}>
-        <Tab label="Apoio a Publicação Científica" />
-        <Tab label="Demanda Extra" />
-      </Tabs>
-
-      {/* Aba normal: Mantida sem alterações */}
-      {currentTab == 0 && <SolicitationTableRequests />}
-      
-      {/* Aba Extra: Agora recebe a função onShowDetails */}
-      {currentTab == 1 && (
-        <SolicitationTableExtraRequests onShowDetails={onShowDetails} />
-      )}
+      <SolicitationTableRequests />
     </>
   );
 }

a7dd548451c9f1e6ee2505c2b88028d45c6f9ced
Nadson Santana <nadson.sousa@ufba.br>
2026-08-03 00:32:01 -0300

    feat: change db migration

--- proap-api/src/main/resources/db/migration/V4__update_extrarequest.sql
+++ proap-api/src/main/resources/db/migration/V4__update_extrarequest.sql
@@ -1,15 +1,9 @@
-ALTER TABLE proap.proap_extra_request
-ADD COLUMN avaliador_ceapg_id BIGINT;
+ALTER TABLE proap.proap_extra_request 
+ADD COLUMN IF NOT EXISTS avaliador_ceapg_id BIGINT,
+ADD COLUMN IF NOT EXISTS custo_final_ceapg NUMERIC(19, 4),
+ADD COLUMN IF NOT EXISTS observacoes_ceapg TEXT,
+ADD COLUMN IF NOT EXISTS data_avaliacao_ceapg DATE;
 
-ALTER TABLE proap.proap_extra_request
-ADD COLUMN custo_final_ceapg NUMERIC(19, 4);
-
-ALTER TABLE proap.proap_extra_request
-ADD COLUMN observacoes_ceapg TEXT;
-
-ALTER TABLE proap.proap_extra_request
-ADD COLUMN data_avaliacao_ceapg DATE;
-
-ALTER TABLE proap.proap_extra_request
-ADD CONSTRAINT fk_extra_request_avaliador_ceapg
-FOREIGN KEY (avaliador_ceapg_id) REFERENCES proap.user(id);
+ALTER TABLE proap.proap_extra_request 
+ADD CONSTRAINT fk_extra_request_avaliador_ceapg 
+FOREIGN KEY (avaliador_ceapg_id) REFERENCES proap.aut_user(id);
